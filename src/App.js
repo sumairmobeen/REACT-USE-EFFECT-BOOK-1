5c929b33b55f374d7a4ef9dc491839d27dd05834
SaharSuleman <qazirameezahmed569@gmail.com>
2021-02-15 17:44:26 +0500

    use-feect

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import React from "react";
+
+
+function App(){
+
+const [posts, setPosts] = React.useState([]);
+const [click, setClick] = React.useState([true]);
+
+  React.useEffect(() => {
+
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+        setPosts(newPosts);
+      });
+  }, [click]);
+
+  return (<div>
+    <h1>/r/reactjs</h1>
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  </div>
+
+  );
+}
+
+export default App;
